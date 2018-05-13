a2e7e2c2650fa4d631ff17fb74159fb01ddbf110
techbuddynaveen <techbuddy144@gmial.com>
2018-05-13 23:14:07 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'chatscreen.dart';
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("FAKEBOOK"),     
+      ),
+      body: new Chatscreen()
+    );
+  }
+}
